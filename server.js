e55b2910815c3b11901cdf9b27d889b396b3eb63
Hoshhh <nyxious123@live.com>
2022-10-09 16:40:56 -0400

    added some dependencies

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://127.0.0.1:27017/blog')
@@ -8,13 +10,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/blog')
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date,
-        description: 'Test description'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
     res.render("articles/index", {articles: articles})
 })
 
